53f9930e67036be38c9ac605db48b703475f3eae
PauloBarros-creator <62844951+PauloBarros-creator@users.noreply.github.com>
2021-03-21 19:35:21 +0000

    BD(incompleto)

--- App/App/Startup.cs
+++ App/App/Startup.cs
@@ -30,6 +30,7 @@ namespace App
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+                    
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
