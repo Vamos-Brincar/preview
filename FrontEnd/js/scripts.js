de9b071bb05d246665ca8a92ef54b66af501edb4
Nuno <onunomoreira@gmail.com>
2021-04-09 12:01:42 +0100

    Menu Jogos atualizado com jogos

--- FrontEnd/js/scripts.js
+++ FrontEnd/js/scripts.js
@@ -64,6 +64,34 @@
         $(this).removeClass("floating-label-form-group-with-focus");
       });
     });
-  
+
+    // Get the modal
+    var modal = document.getElementById('gamebox');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("playgame");
+
+
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    var gamebox = document.getElementById("close");
+
+    gamebox.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+
+
   })(jQuery); // End of use strict
   
