4a085246f5f20878122ee09a79bdacb9ad65b2d5
PedroMM-G <a92930@alunos.uminho.pt>
2021-04-11 12:08:28 +0100

    crianca atividade model e correçao do model crianca

--- Vamos Brincar/Models/Criança.cs
+++ Vamos Brincar/Models/Criança.cs
@@ -4,13 +4,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
 {
-    public class Movie
+    public class Crianca
     {
         public int id_crianca { get; set; }
         public string nome { get; set; }
 
         public int idade { get; set; }
-        public string passs { get; set; }
+        public string pass { get; set; }
     }
 }
 //Login
---
+++ Vamos Brincar/Models/crianca_ativ.cs
@@ -0,0 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class crianca_ativ
+    {
+        public int id_crianca_ativ { get; set; }
+        public string id_crianca { get; set; }
+        public int id_atividade { get; set; }
+    }
+}
