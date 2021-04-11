74b8e3969ee78cea815bde4a00df09eaffd32d2b
PedroMM-G <a92930@alunos.uminho.pt>
2021-04-11 11:34:22 +0100

    model da criança e add das tabelas

--- Vamos Brincar/Models/Criança.cs
+++ Vamos Brincar/Models/Criança.cs
@@ -1,3 +1,16 @@
 //Register
+using System;
+using System.ComponentModel.DataAnnotations;
 
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int id_crianca { get; set; }
+        public string nome { get; set; }
+
+        public int idade { get; set; }
+        public string passs { get; set; }
+    }
+}
 //Login
